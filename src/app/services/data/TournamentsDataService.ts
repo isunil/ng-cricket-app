9783472ad4a3010da08844f28589a76bc320322c
Sunil_Nuthalapati <Sunil.Nuthalapati@gmail.com>
2019-09-09 22:28:28 -0500

    updates to packages and 2019 T10.

--- src/app/services/data/TournamentsDataService.ts
+++ src/app/services/data/TournamentsDataService.ts
@@ -4,64 +4,87 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TournamentsDataService {
-    constructor() { }
+  constructor() { }
 
-    public getTournamentData(): Observable<string> {
-        const data = {
-            'tournaments': [
-                {
-                    'year': '2017',
-                    'tournaments': [
-                        {
-                            'type': '16',
-                            'details': {
-                                'organizationId': '1091',
-                                'competitionId': '10685',
-                                'clubId': '216136',
-                                'drawId': '32545'
-                            }
-                        },
-                        {
-                            'type': '10',
-                            'details': {
-                                'organizationId': '1091',
-                                'competitionId': '11075',
-                                'clubId': '216136',
-                                'drawId': '33897'
-                            }
-                        }
-                    ]
-                },
-                {
-                    'year': '2018',
-                    'tournaments': [
-                        {
-                            'type': '16',
-                            'details': {
-                                'organizationId': '1091',
-                                'competitionId': '12135',
-                                'clubId': '216136',
-                                'drawId': '38518'
-                            }
-                        },
-                        {
-                          'type': '10',
-                          'details': {
-                              'organizationId': '1091',
-                              'competitionId': '12351',
-                              'clubId': '216136',
-                              'drawId': '39296'
-                          }
-                      }
-                    ]
-                }
-            ]
-        };
-        return new Observable((observer) => {
-            // observable execution
-            observer.next(JSON.stringify(data));
-            observer.complete();
-        });
-    }
+  public getTournamentData(): Observable<string> {
+    const data = {
+      'tournaments': [
+        {
+          'year': '2017',
+          'tournaments': [
+            {
+              'type': '16',
+              'details': {
+                'organizationId': '1091',
+                'competitionId': '10685',
+                'clubId': '216136',
+                'drawId': '32545'
+              }
+            },
+            {
+              'type': '10',
+              'details': {
+                'organizationId': '1091',
+                'competitionId': '11075',
+                'clubId': '216136',
+                'drawId': '33897'
+              }
+            }
+          ]
+        },
+        {
+          'year': '2018',
+          'tournaments': [
+            {
+              'type': '16',
+              'details': {
+                'organizationId': '1091',
+                'competitionId': '12135',
+                'clubId': '216136',
+                'drawId': '38518'
+              }
+            },
+            {
+              'type': '10',
+              'details': {
+                'organizationId': '1091',
+                'competitionId': '12351',
+                'clubId': '216136',
+                'drawId': '39296'
+              }
+            }
+          ]
+        },
+        {
+          'year': '2019',
+          'tournaments': [
+            {
+              'type': '16',
+              'details': {
+                'organizationId': '1091',
+                'competitionId': '13048',
+                'clubId': '216136',
+                'drawId': '43582'
+              }
+            },
+            {
+              'type': '10',
+              'details': {
+                  'organizationId': '1091',
+                  'competitionId': '13048',
+                  'clubId': '216136',
+                  'drawId': '43582'
+              }
+          }
+          ]
+        }
+      ]
+    };
+    return new Observable((observer) => {
+      // observable execution
+      observer.next(JSON.stringify(data));
+      observer.complete();
+    });
+  }
 }
 
